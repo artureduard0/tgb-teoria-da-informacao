1b4033edf0056bedc7c7ac4027388c00fb00e1db
Leonardo <leonardorteixeira@hotmail.com>
2021-11-21 17:35:36 -0300

    Permutação tabela 1 nos blocos de 48 bits

--- src/App.java
+++ src/App.java
@@ -66,7 +66,7 @@ public class App {
 
                         for (int i = 0; i < blocos.size(); i++) {
                             for (int j = 0; j < blocos.get(i).length; j++) {
-                                System.out.println((int) blocos.get(i)[j]);
+                                // System.out.println((int) blocos.get(i)[j]);
                             }
                         }
 
@@ -118,16 +118,16 @@ public class App {
 
     private static String[] keyScheduling(String key) {
         byte[] keyBytes = key.getBytes();
-        System.out.println("-----------------------------------");
-        System.out.println("Bytes: " + Arrays.toString(keyBytes));
+        // System.out.println("-----------------------------------");
+        // System.out.println("Bytes: " + Arrays.toString(keyBytes));
 
         String keyBinary = "";
         for (int i = 0; i < keyBytes.length; i++) {
             String s = toBinary(keyBytes[i], 8);
-            System.out.println("Byte posição " + i + " em binário: " + s);
+            // System.out.println("Byte posição " + i + " em binário: " + s);
             keyBinary += s;
         }
-        System.out.println("\nChave em binário: " + keyBinary);
+        // System.out.println("\nChave em binário: " + keyBinary);
 
         int[] table = { 32, 28, 24, 20, 16, 12, 8, 4, 29, 25, 21, 17, 13, 9, 5, 1, 31, 27, 23, 19, 15, 11, 7, 3, 30, 26,
                 22, 18, 14, 10, 6, 2 };
@@ -136,36 +136,36 @@ public class App {
         String permutedKey = permutation(keyBinary, table, false, 0);
 
         String[] strings = split(permutedKey);
-        System.out.println("-----------------------------------");
-        System.out.println("Chaves em 8 bits: " + Arrays.toString(strings));
+        // System.out.println("-----------------------------------");
+        // System.out.println("Chaves em 8 bits: " + Arrays.toString(strings));
 
         int[] values = new int[4];
         for (int i = 0; i < values.length; i++) {
             values[i] = Integer.parseInt(strings[i], 2);
         }
-        System.out.println("Chaves em inteiros: " + Arrays.toString(values));
+        // System.out.println("Chaves em inteiros: " + Arrays.toString(values));
 
         strings = rightShift(values, 8);
 
         int[] expansionTable = { 8, 1, 5, 2, 4, 6, 5, 2, 7, 1, 8, 7, 6, 3, 3, 4 };
-        System.out.println("-----------------------------------");
+        // System.out.println("-----------------------------------");
         for (int i = 0; i < strings.length; i++) {
-            System.out.println("Subchave " + i + ", expansão de 8 > 16 bits");
+            // System.out.println("Subchave " + i + ", expansão de 8 > 16 bits");
             strings[i] = permutation(strings[i], expansionTable, true, 16);
         }
 
-        System.out.println("-----------------------------------");
+        // System.out.println("-----------------------------------");
         for (int i = 0; i < values.length; i++) {
             values[i] = Integer.parseInt(strings[i], 2);
         }
-        System.out.println("Subchaves em inteiros: " + Arrays.toString(values));
+        // System.out.println("Subchaves em inteiros: " + Arrays.toString(values));
 
         strings = rightShift(values, 16);
 
         int[] expansionTable2 = { 9, 15, 7, 3, 5, 11, 13, 1, 2, 4, 9, 10, 12, 16, 5, 3, 2, 10, 7, 11, 8, 6, 14, 9 };
-        System.out.println("-----------------------------------");
+        // System.out.println("-----------------------------------");
         for (int i = 0; i < strings.length; i++) {
-            System.out.println("Subchave " + i + ", expansão de 16 > 24 bits");
+            // System.out.println("Subchave " + i + ", expansão de 16 > 24 bits");
             strings[i] = permutation(strings[i], expansionTable2, true, 24);
         }
 
@@ -195,26 +195,51 @@ public class App {
             }
         }
 
-        System.out.println("Chave permutada:  " + sPermuted.toString());
+        // System.out.println("Chave permutada:  " + sPermuted.toString());
         return sPermuted.toString();
     }
 
     private static String[] rightShift(int[] values, int size) {
         String[] strings = new String[4];
-        System.out.println("-----------------------------------");
+        // System.out.println("-----------------------------------");
         for (int i = 0; i < values.length; i++) {
             int n = values[i];
             n = (n >>> 1) | (n << (size - 1));
             String s = toBinary(n, size);
             s = s.substring(s.length() - size);
             strings[i] = s;
-            System.out.println("Subchave " + i + " após >>> " + strings[i]);
+            // System.out.println("Subchave " + i + " após >>> " + strings[i]);
         }
         return strings;
     }
 
     private static void encrypt(ArrayList<byte[]> blocos) {
-        int[] t1 = {};
+        // * Permutação inicial
+        int[] t1 = {43, 37, 31, 25, 19, 13, 7,  1,
+                    47, 41, 35, 29, 23, 17, 11, 5,
+                    45, 39, 33, 27, 21, 15, 9,  3,
+                    48, 42, 36, 30, 24, 18, 12, 6,
+                    44, 38, 32, 26, 20, 14, 8,  2,
+                    46, 40, 34, 28, 22, 16, 10, 4,
+        };
+
+        ArrayList<String> blocosPermutados = new ArrayList<>();
+
+        for (int i = 0; i < blocos.size(); i++) {
+            byte[] bloco = blocos.get(i);
+            String blockBinary = "";
+            for (int j = 0; j < bloco.length; j++) {
+                String s = toBinary(bloco[j], 8);
+                blockBinary += s;
+            }
+            blocosPermutados.add(permutation(blockBinary, t1, false, 48));
+            // System.out.println("Bloco " + i + " em binário: " + blockBinary);
+            // System.out.println("Bloco " + i + " permutado em binário: " + blocosPermutados.get(i));
+        }
+        
+        
+
+
         boolean inverter = false;
 
         ArrayList<byte[]> blocosEncriptados = new ArrayList<>();
